a07a1400c7dfd79ee5f5fe76478cd2805e560d2f
ICKelin <yingjiu.hulu@gmail.com>
2021-04-12 19:01:49 +0800

    feature: use share dict

--- lua/core/upstream.lua
+++ lua/core/upstream.lua
@@ -3,37 +3,22 @@ local ngx = ngx
 local pcall = pcall
 local log = require("comm.log")
 local new_tab = require("table.new")
+local decode_json = require("cjson.safe").decode
+local encode_json = require("cjson.safe").encode
 
 local _M = {}
-local upstreams = {}
-
-do 
-    local running
-
-function _M.print_stat(premature)
-    if premature or running then
-        return
-    end
-
-    running = true
-    for k, v  in pairs(upstreams) do
-        log.info(k, "=>", v.ip, ":", v.port)
-    end
-    running = false
-end
-
-end -- do
+local upstreams = ngx.shared.upstreams
 
 
 function _M.get_node(host) 
-    return upstreams[host]
+    local val = upstreams:get(host)
+    log.info("host ", host, " node info: ", val)
+    return decode_json(val)
 end
 
 
 function _M.del_node(host)
-    if upstreams[host] then
-        upstreams[host] = nil
-    end
+    upstreams:delete(host)
 end
 
 
@@ -41,13 +26,13 @@ function _M.set_node(node_info)
     local host = node_info.host
     local ip = node_info.ip
     local port = node_info.port
+    local value = encode_json(node_info)
     log.info("host: ", host, " ip: ", ip, " port: ", port)
-    upstreams[host] = node_info
+    upstreams:set(host, value)
 end
 
 
 function _M.init_worker()
-    ngx.timer.every(10, _M.print_stat)
 end
 
 
